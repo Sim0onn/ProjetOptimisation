c1b35d22bd50d97ece7ebcb6b56b48a3e89f879d
LeRubz <rubensgzmn1012@gmail.com>
2025-10-15 12:07:07 +0200

    génération des graphes en fonction des tranches horaires et calcul du poids des trajets

--- src/utils/traffic.utils.py
+++ src/utils/traffic.utils.py
@@ -8,46 +8,61 @@ coeffFluide = 1
 coeffModere = 1.15
 coeffLourd = 1.3
 
-importanceCout = 1
+importanceTemps = 1
 importanceConso = 1
 
 def generateTrafficGraphs(base_graph):
+    trafficGraphs = []
+
     for i in range(11):
+        trafficGraph = Graph()
+
         match i:
-            case 0: #traffic a 8 heures
-                congestionTraffic = random.random(1.2,1.5)
-            case 1: #traffic a 9 heures
-                congestionTraffic = random.random(1.1,1.4)
-            case 2: #traffic a 10 heures
-                congestionTraffic = random.random(1,1.2)
-            case 3: #traffic a 11 heures
-                congestionTraffic = random.random(1,1.2)
-            case 4: #traffic a 12 heures
-                congestionTraffic = random.random(1.1,1.3)
-            case 5: #traffic a 13 heures
-                congestionTraffic = random.random(1,1.2)
-            case 6: #traffic a 14 heures
-                congestionTraffic = random.random(1,1.1)
-            case 7: #traffic a 15 heures
-                congestionTraffic = random.random(1,1.3)
-            case 8: #traffic a 16 heures
-                congestionTraffic = random.random(1.1,1.4)
-            case 9: #traffic a 17 heures
-                congestionTraffic = random.random(1.2,1.5)
-            case 10: #traffic a 18 heures
-                congestionTraffic = random.random(1.1,1.4)
+            case 0:  # 8h
+                congestionMin, congestionMax = 1.2, 1.5
+            case 1:  # 9h
+                congestionMin, congestionMax = 1.1, 1.4
+            case 2:  # 10h
+                congestionMin, congestionMax = 1.0, 1.2
+            case 3:  # 11h
+                congestionMin, congestionMax = 1.0, 1.2
+            case 4:  # 12h
+                congestionMin, congestionMax = 1.1, 1.3
+            case 5:  # 13h
+                congestionMin, congestionMax = 1.0, 1.2
+            case 6:  # 14h
+                congestionMin, congestionMax = 1.0, 1.1
+            case 7:  # 15h
+                congestionMin, congestionMax = 1.0, 1.3
+            case 8:  # 16h
+                congestionMin, congestionMax = 1.1, 1.4
+            case 9:  # 17h
+                congestionMin, congestionMax = 1.2, 1.5
+            case 10:  # 18h
+                congestionMin, congestionMax = 1.1, 1.4
 
-        tempsTrajet = distance/vitesseBase * congestionTraffic
 
-        match congestionTraffic:
-            case x if x < 1.2:
-                consoTrajet = distance*consoBase * coeffFluide * congestionTraffic
-            case x if 1.2 <= x < 1.4:
-                consoTrajet = distance*consoBase * coeffModere * congestionTraffic
-            case x if x >= 1.4:
-                consoTrajet = distance*consoBase * coeffLourd * congestionTraffic
-            case _:
+        for city1, city2, distance in base_graph.getEdges():
+            congestionTraffic = random.uniform(congestionMin, congestionMax)
 
+            tempsTrajet = (distance / vitesseBase) * congestionTraffic
+
+            if congestionTraffic < 1.2:
+                consoTrajet = distance * consoBase * coeffFluide * congestionTraffic
+            elif congestionTraffic < 1.4:
+                consoTrajet = distance * consoBase * coeffModere * congestionTraffic
+            else:
+                consoTrajet = distance * consoBase * coeffLourd * congestionTraffic
+
+            nouveauPoids = (importanceTemps * tempsTrajet) + (importanceConso * consoTrajet)
+
+            trafficGraph.addCity(city1)
+            trafficGraph.addCity(city2)
+            trafficGraph.addEdge(city1, city2, nouveauPoids)
+
+        trafficGraphs.append(trafficGraph)
+
+    return trafficGraphs
 
                 
                 
