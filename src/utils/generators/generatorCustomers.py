61e6a315231b1951c99ba87e907810f563f68836
Sim0onn <sim85340@gmail.com>
2025-11-07 09:32:31 +0100

    Modification des CSV graphs pour graphes orientés + modification customres pour prendre dans tt le fichier et pas seuleemnt 10 objs

--- src/utils/generators/generatorCustomers.py
+++ src/utils/generators/generatorCustomers.py
@@ -13,13 +13,21 @@ random.seed(SEED)
 def generatorCustomers(nb, nb_it=NB_INSTANCES):
     customer = Customer(f'client_{nb}')
 
+    # Lecture du fichier complet, sans limitation à 10 lignes
     df = pd.read_csv(f'fixtures/objects/objects_{nb_it}.csv')
-    selected_df = df.iloc[:10]
-    n_objets = random.randint(3, 5)  
-    indices = random.sample(range(len(selected_df)), n_objets) if len(selected_df) >= n_objets else list(range(len(selected_df)))
-    sample_df = selected_df.iloc[indices]
+
+    # Nombre d'objets à attribuer au client
+    n_objets = random.randint(3, 5)
+
+    # Tirage aléatoire dans tout le fichier
+    indices = random.sample(range(len(df)), n_objets) if len(df) >= n_objets else list(range(len(df)))
+    sample_df = df.iloc[indices]
+
+    # Transformation en liste de tuples (type, name)
     tableau_tuples = list(sample_df.itertuples(index=False, name=None))
 
+    # Ajout des objets au client
     for type, name in tableau_tuples:
         customer.addObject(type, name)
+
     return customer
